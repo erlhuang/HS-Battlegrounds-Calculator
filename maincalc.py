56c6bb39b5e63138d948d35f16482d3612e64082
Eric Huang <erlhuang@ucsc.edu>
2020-01-16 16:28:01 -0800

    modified json files, starting with basic logic for battle simulator

--- maincalc.py
+++ maincalc.py
@@ -1,14 +1,36 @@
 import requests
+import json
 
-url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/Battlegrounds"
 data = {}
-querystring = {"cost":"2"}
 
-headers = {
-    'x-rapidapi-host': "omgvamp-hearthstone-v1.p.rapidapi.com",
-    'x-rapidapi-key': "dce5f008a7msh663c93cb6f5940ap102dbejsn84c97939d703"
-    }
+playerBoard = []
+opponentBoard = []
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+#A doubly linked list connecting all minions on a board state 
+#Lot to implement( Logic handling for deathrattles, figuring out how to implement order, etc)
+#Deathrattle is tricky 
+#Other effects: Windfury, poison 
+class Minions():
+    def __init__(self, name, attack, hp, golden, shield, poison, windfury):
+        self.name = name 
+        self.attack = attack
+        self.hp = hp
+        self.golden = golden 
+        self.shield = shield 
+        self.poison = poison 
+        self.windfury = windfury 
+    
 
-print(response.text)
+tideHunter = Minions('Murloc Tidehunter',2,1, false, false, false, false )
+playerBoard.append(tideHunter)
+
+#Edge cases to account for:
+# microbots on already existing Deathrattles 
+#making sure there is room for deathrattles and we never overflow 
+#how to calculate all possible permutations?
+#for now, just have a simulation of the outcome of one possible battle?
+with open('2cost.json') as json_file:
+    jsonData = json.load(json_file)
+    for p in jsonData:
+        print(p['name'])
+    print(jsonData)
